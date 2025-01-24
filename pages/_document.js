55919f7c5704b89884d26b27fa96f0eb48b74116
Denis-Wanjohi <deniswanjohi15@gmail.com>
2025-01-24 13:43:51 +0300

    Modern Interior Kenya

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <title>Modern Interior Kenya</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" href="/images/Modern-Interior-Kenya-Logo.png" type="image/"></link>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
